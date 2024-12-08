c24b8028e9cf72111bc35ea8a698595c1834f640
denis <denis961111@gmail.com>
2024-12-08 01:05:00 -0800

    add serveStatic

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from 'hono/bun';
 import { watchlistRoute } from "./routes/watchlist";
 
 const app = new Hono();
 
 app.use(logger());
 
-app.get("/test", (c) => {
-  return c.json({ message: "test" });
-});
-
 app.route("/api/watchlist", watchlistRoute)
+
+app.get('*', serveStatic({ root: './client/dist' }))
+app.get('*', serveStatic({ path: './client/dist/index.html' }))
 
 export default app;
