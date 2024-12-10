8de8026fc915599711fce2615fe990165f3bd9e9
denis <denis961111@gmail.com>
2024-12-10 04:51:49 -0800

    add middleware

--- server/kinde.ts
+++ server/kinde.ts
@@ -1,15 +1,24 @@
-import {createKindeServerClient, GrantType, type SessionManager } from "@kinde-oss/kinde-typescript-sdk";
+import {
+  createKindeServerClient,
+  GrantType,
+  type SessionManager,
+  type UserType,
+} from "@kinde-oss/kinde-typescript-sdk";
 import { type Context } from "hono";
 import { getCookie, setCookie, deleteCookie } from "hono/cookie";
+import { createFactory, createMiddleware } from "hono/factory";
 
 // Client for authorization code flow
-export const kindeClient = createKindeServerClient(GrantType.AUTHORIZATION_CODE, {
-  authDomain: process.env.KINDE_DOMAIN!,
-  clientId: process.env.KINDE_CLIENT_ID!,
-  clientSecret: process.env.KINDE_CLIENT_SECRET!,
-  redirectURL: process.env.KINDE_REDIRECT_URI!,
-  logoutRedirectURL: process.env.KINDE_LOGOUT_REDIRECT_URI!
-});
+export const kindeClient = createKindeServerClient(
+  GrantType.AUTHORIZATION_CODE,
+  {
+    authDomain: process.env.KINDE_DOMAIN!,
+    clientId: process.env.KINDE_CLIENT_ID!,
+    clientSecret: process.env.KINDE_CLIENT_SECRET!,
+    redirectURL: process.env.KINDE_REDIRECT_URI!,
+    logoutRedirectURL: process.env.KINDE_LOGOUT_REDIRECT_URI!,
+  }
+);
 
 let store: Record<string, unknown> = {};
 
@@ -34,8 +43,30 @@ export const sessionManager = (c: Context): SessionManager => ({
     deleteCookie(c, key);
   },
   async destroySession() {
-    ["id_token", "access_token", "user", "refresh_token"].forEach((key) =>{
+    ["id_token", "access_token", "user", "refresh_token"].forEach((key) => {
       deleteCookie(c, key);
     });
   },
+});
+
+type Env = {
+  Variables: {
+    user: UserType;
+  };
+};
+
+export const getUser = createMiddleware<Env>(async (c, next) => {
+  try {
+    const manager = sessionManager(c);
+    const isAuthenticated = await kindeClient.isAuthenticated(manager);
+    if (!isAuthenticated) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+    const user = await kindeClient.getUser(manager);
+    c.set("user", user);
+    await next();
+  } catch (e) {
+    console.error(e);
+    return c.json({ error: "Unauthorized" }, 401);
+  }
 });
