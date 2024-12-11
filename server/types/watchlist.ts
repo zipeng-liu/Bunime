4c02be017ba3120067960d52067caed427f479e5
denis <denis961111@gmail.com>
2024-12-10 19:38:46 -0800

    add watchlist to db

--- server/types/watchlist.ts
+++ server/types/watchlist.ts
@@ -6,8 +6,8 @@ export const watchlistSchema = z.object({
   user_id: z.string().nonempty(), // User ID as text
   anime_id: z.number().int().positive(), // Anime ID as integer
   progress: z.string().nullable().optional(), // Progress as a nullable or optional string
-  status: z.enum(["Not Started", "In Progress", "Completed", "Paused"]), // Status as an enum
-  type: z.enum(["TV", "Movie", "OVA", "ONA", "Special"]), // Type as an enum
+  status: z.string().min(1), // Status as an enum
+  type: z.string().min(1), // Type as an enum
 });
 
 // Type inferred from watchlist schema
@@ -18,9 +18,11 @@ export const addWatchlistSchema = z.object({
   user_id: z.string().nonempty(), // User ID as text
   anime_id: z.number().int().positive(), // Anime ID as integer
   progress: z.string().nullable().optional(), // Progress is nullable or optional
-  status: z.enum(["Not Started", "In Progress", "Completed", "Paused"]), // Status as an enum
-  type: z.enum(["TV", "Movie", "OVA", "ONA", "Special"]), // Type as an enum
+  status: z.string().min(1), // Status as an enum
+  type: z.string().min(1), // Type as an enum
 });
+
+export type AddWatchlist = z.infer<typeof addWatchlistSchema>;
 
 // Schema for updating a watchlist item
 export const updateWatchlistSchema = z.object({
